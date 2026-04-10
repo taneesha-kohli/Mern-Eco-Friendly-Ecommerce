de01e360892975eb107c133e926e956515b84da4
taneesha-kohli <shivanirana1245@gmail.com>
2026-04-10 18:46:29 +0530

    modified new domain in cors origin

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://mern-eco-friendly-ecommerce.vercel.app',
+  origin: 'https://shivani-kohli-ecomarketplace.vercel.app',
   credentials: true
 }))
 
